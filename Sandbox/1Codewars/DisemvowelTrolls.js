f450520bb2c3b8e64da9ab3840c83b86c6d50062
Warnd <dzerdevwarnd@gmail.com>
2023-08-30 22:30:41 +0300

    4

--- Sandbox/1Codewars/DisemvowelTrolls.js
+++ Sandbox/1Codewars/DisemvowelTrolls.js
@@ -1,5 +1,4 @@
 function disemvowel(str) {
-	str = str.toLowerCase()
 	let newStr = ''
 	for (let i = 0; i < str.length; i++)
 		if (
@@ -7,7 +6,12 @@ function disemvowel(str) {
 			str[i] !== 'e' &&
 			str[i] !== 'i' &&
 			str[i] !== 'o' &&
-			str[i] !== 'u'
+			str[i] !== 'u' &&
+			str[i] !== 'O' &&
+			str[i] !== 'A' &&
+			str[i] !== 'E' &&
+			str[i] !== 'I' &&
+			str[i] !== 'U'
 		) {
 			newStr += str[i]
 		}
