040f776955c0cec0425d5b34a0c8887540a90a2a
Dzerdevwarnd <dzerdevwarnd3@gmail.com>
2023-09-01 10:03:38 +0300

    Square

---
+++ Sandbox/SquareEvery.js
@@ -0,0 +1,9 @@
+function squareDigits(num) {
+	str = num.toString()
+	let newStr = ''
+	for (let i = 0; i < str.length; i++) {
+		newStr += str[i] * str[i]
+	}
+	let newNum = Number(newStr)
+	return newStr
+}
